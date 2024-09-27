d90ca27c2aaab921a6ea4dd8ff5659d1082e5843
sanket shiwakoti <sanketshiwakoti2@gmail.com>
2024-09-27 16:53:52 +0545

    marketplace index page route and controller added

--- Web/app/Http/Controllers/ProductController.php
+++ Web/app/Http/Controllers/ProductController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = Product::with('user')->get(); // Fetch all products with their associated user
+        return view('marketplace.index', compact('products'));
+    }
 }
