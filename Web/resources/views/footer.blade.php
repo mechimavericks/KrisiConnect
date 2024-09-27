2b2a17b33a791352d45dddfc5b363f13ea53f391
sanket shiwakoti <sanketshiwakoti2@gmail.com>
2024-09-27 15:31:40 +0545

    footer nav bar changed into component

---
+++ Web/resources/views/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="bg-[#1e2945] flex justify-around py-3 rounded-2xl fixed bottom-5 left-0 right-0 mx-auto max-w-sm">
+    <div class="text-3xl"><a href="/"><i class="bx bx-home"></i></a></div>
+    <div class="text-3xl"><a href="/scan"><i class="bx bx-scan"></i></a></div>
+    <div class="text-3xl"><a href="/marketplace"><i class="bx bx-store-alt"></i></a></div>
+    <div class="text-3xl"><a href="http://localhost:8000/chat"><i class="bx bxs-bot"></i></a></div>
+</footer>
