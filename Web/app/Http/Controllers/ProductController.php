58dba23615c526f3110d0520f8a0f2a6419418c5
sanket shiwakoti <sanketshiwakoti2@gmail.com>
2024-09-27 18:22:36 +0545

    product stored succesfully

--- Web/app/Http/Controllers/ProductController.php
+++ Web/app/Http/Controllers/ProductController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+
 
 class ProductController extends Controller
 {
@@ -12,8 +15,34 @@ class ProductController extends Controller
         $products = Product::all(); 
         return view('marketplace.index', compact('products'));
     }
+
     public function create()
     {
         return view('marketplace.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'price' => 'required|numeric',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+
+        $product = new Product();
+        $product->name = $request->input('name');
+        $product->description = $request->input('description');
+        $product->price = $request->input('price');
+        $product->user_id = Auth::id();
+
+        if ($request->hasFile('image')) {
+            $imagePath = $request->file('image')->store('public/products');
+            $product->image = str_replace('public/', '', $imagePath);
+        }
+
+        $product->save();
+
+        return redirect()->route('marketplace.index')->with('success', 'Product created successfully!');
     }
 }
