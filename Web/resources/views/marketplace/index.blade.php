c2eaf66abe9adb62b530930c3f25efc8a7d5507a
sanket shiwakoti <sanketshiwakoti2@gmail.com>
2024-09-27 15:49:52 +0545

    marketplace index page setup

---
+++ Web/resources/views/marketplace/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ne">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>कृषि कनेक्ट</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+</head>
+<body class="bg-[#0d1727] text-white font-sans">
+
+
+
+ 
+
+
+
+
+
+</body>
+</html>
