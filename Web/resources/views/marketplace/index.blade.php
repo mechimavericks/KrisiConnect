00567eb57670e17f8b3448ddbdb817077c5d2efc
sanket shiwakoti <sanketshiwakoti2@gmail.com>
2024-09-27 15:54:03 +0545

    header added in marketplace index page

--- Web/resources/views/marketplace/index.blade.php
+++ Web/resources/views/marketplace/index.blade.php
@@ -9,7 +9,17 @@
 </head>
 <body class="bg-[#0d1727] text-white font-sans">
 
+<div class="max-w-lg mx-auto p-5">
+    <header class="flex justify-between items-center mb-5">
+        <h1 class="text-xl font-bold">कृषि कनेक्ट</h1>
+    </header>
+    
+   
 
+    
+
+    @include('footer')
+</div>
 
  
 
