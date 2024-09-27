ea26a4812547df9a4fc7db0c12b5303e8e3c368e
sameer-kharel <shameerkharel@gmail.com>
2024-09-27 17:47:41 +0545

    made the template for the test for the model

--- Python/disease/main.py
+++ Python/disease/main.py
@@ -1,7 +1,24 @@
+from flask import Flask, request, render_template, send_file
 from ultralytics import YOLO
+import os
 
+app = Flask(__name__)
 model = YOLO("last.pt")
 
-test_img = "image.png"
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            file_path = os.path.join('uploads', file.filename)
+            file.save(file_path)
+            
+            results = model(source=file_path, show=False, save=True, conf=0.5)
+            detected_img_path = results.save_dir / file.filename
+            
+            return send_file(detected_img_path, mimetype='image/png')
+    return render_template('upload.html')
 
-results = model(source=test_img, show=True, save=True, conf=0.5)
+
+if __name__ == '__main__':
+    app.run(port=49)
